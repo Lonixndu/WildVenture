a6ffc0b490061b04eb38dec377c851afb2379663
Lonixndu <163233270+Lonixndu@users.noreply.github.com>
2024-04-25 23:53:34 +0530

    edited slideshow to auto transition

--- templates/slideshow/slideshow.js
+++ templates/slideshow/slideshow.js
@@ -1,18 +1,43 @@
-let sIndex = 1;
+var sIndex = 1;
+var slideInterval;
 showSlide(sIndex);
+startSlideShow();
+
+
+//function to start automatic slideshow
+function startSlideShow(){
+    slideInterval = setInterval(
+        function() {nextSlide(1); //move to next slide
+    }, 3000 //in miliseconds
+    );
+}
+
+//function to stop automatic slideshow
+function stopSlideShow(){
+    clearInterval(slideInterval);
+}
+
+
 
 //next or previous slide change
 function nextSlide(n)
 {
+    stopSlideShow();//stop auto slideshow
     showSlide(sIndex += n);
 }
+
+
 
 //image controls by clicking dot
 function currSlide(n)
 {
+    stopSlideShow(); //stop auto slideshow
     showSlide(sIndex = n);
 }
 
+
+
+//function that displays the certain image
 function showSlide(n)
 {
     let i;
@@ -38,4 +63,6 @@ function showSlide(n)
 
     slides[sIndex - 1].style.display = "block";
     dots[sIndex - 1].className += " slide-active";
+
+    startSlideShow(); //restart auto slideshow if no usr interact
 }
