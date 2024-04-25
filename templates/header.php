8e1f03c8e489b82c2ee946ab482b1917cbe99ccf
Lonixndu <163233270+Lonixndu@users.noreply.github.com>
2024-04-25 17:51:06 +0530

    templates

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <style>
+        #logo{height: 150px; width: auto; float: left;}
+        #heading{text-align: center; font-size: 50px; font-weight: bold;}
+        #subHeading{text-align: center; font-size: 20px; }
+    </style>
+</head>
+
+<body>
+    <div>
+        <img src="./logo.jpeg" id="logo">
+        <h1 id="heading">WildVenture</h1>
+        <h4 id="subHeading">Wildlife adventure organizer</h4>
+    </div>
