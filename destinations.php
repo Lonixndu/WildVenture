7ccd4863f483c172a02cc07be522dff178ff38b2
Lonixndu <163233270+Lonixndu@users.noreply.github.com>
2024-05-06 00:43:34 +0530

    destinations redirect

--- destinations.php
+++ destinations.php
@@ -33,21 +33,27 @@
         <img src="./templates/destination-pictures/galle.jpg" alt="Galle, Sri Lanka">
         <h2>Galle, Sri Lanka</h2>
         <p>Galle is a historic city located on the southwestern tip of Sri Lanka. Its colonial charm, fortified walls, and vibrant atmosphere make it a must-visit destination. Explore the Dutch Fort, stroll along the cobblestone streets, and indulge in delicious local cuisine.</p>
-        <button class="btn-lrn-mre">Learn More</button>
+        <a href="https://en.wikipedia.org/wiki/Galle">
+                <button class="btn-lrn-mre">Learn More</button>
+        </a>
         </div>
 
         <div class="destination">
             <img src="./templates/destination-pictures/yala.jpg" alt="Yala National Park, Sri Lanka">
             <h2>Yala National Park</h2>
             <p>Yala National Park is renowned for its rich biodiversity and is home to a variety of wildlife species, including leopards, elephants, and sloth bears. Embark on an exciting safari adventure through its diverse landscapes and immerse yourself in nature's wonders.</p>
-            <button class="btn-lrn-mre">Learn More</button>
+            <a href="https://en.wikipedia.org/wiki/Yala_National_Park">
+                <button class="btn-lrn-mre">Learn More</button>
+            </a>
         </div>
 
         <div class="destination">
             <img src="./templates/destination-pictures/amazon.jpg" alt="Kandy, Sri Lanka">
             <h2>Amazon Rainforest</h2>
             <p>Spanning millions of square kilometers, the Amazon rainforest is Earth's crown jewel of biodiversity.  Lush and wet, it shelters ten percent of the world's known species and plays a key role in regulating our planet's climate. </p>
-            <button class="btn-lrn-mre">Learn More</button>
+            <a href="https://en.wikipedia.org/wiki/Amazon_rainforest">
+                <button class="btn-lrn-mre">Learn More</button>
+            </a>
         </div>
     </div>
 
